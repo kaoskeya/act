1444d1583aadfdb489933baebc1b8dc168114ec2
Deepu T Philip <deepu.dtp@gmail.com>
2014-09-14 16:06:23 +0530

    Unfollow

--- webapp/server/methods/subscribe.js
+++ webapp/server/methods/subscribe.js
@@ -4,6 +4,20 @@
 
 Meteor.methods({ 
   '/app/subscribe': function (oid) {
-    Subscriptions.insert({'user_id' : this.userId, oid : oid})
+    var rec = Subscriptions.findOne({
+        user_id : this.userId,
+        oid : oid
+    })
+
+    if(!_.isEmpty(rec)){
+
+        Subscriptions.remove({'user_id' : this.userId, oid : oid})
+
+    }
+    else{
+        Subscriptions.insert({'user_id' : this.userId, oid : oid})
+    }
+
+
   }
 });
